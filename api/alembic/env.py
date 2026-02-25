b887393d7d48095319f1ab02808c3999d5a66920
Joséphine Wolf Oberholtzer <josephine.wolf.oberholtzer@gmail.com>
2026-02-25 13:12:19 -0500

    tinkering

--- api/alembic/env.py
+++ api/alembic/env.py
@@ -1,9 +1,9 @@
 from logging.config import fileConfig
 
-from maps.models import Base
 from sqlalchemy import engine_from_config, pool
 
 from alembic import context
+from maps.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
